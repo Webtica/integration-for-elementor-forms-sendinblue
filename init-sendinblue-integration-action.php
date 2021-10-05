7d0bab8d85f16ed458c1ddbeb99ab22e280f0489
Webtica <info@webtica.be>
2021-10-05 20:15:36 +0200

    Start API settings page setup

--- init-sendinblue-integration-action.php
+++ init-sendinblue-integration-action.php
@@ -8,6 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action( 'elementor_pro/init', function() {
 	// Here its safe to include our action class file
 	include_once( dirname(__FILE__).'/includes/class-sendinblue-integration-action.php' );
+	include_once( dirname(__FILE__).'/includes/integration-settings-page.php' );
 
 	// Instantiate the action class
 	$sendinblue_integration_action = new Sendinblue_Integration_Action_After_Submit();
